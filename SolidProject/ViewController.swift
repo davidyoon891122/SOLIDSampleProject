2c6dd68acc7718d4ae157632fa6fff2f27f3a459
Yoonjiwon <k112751@koreainvestment.com>
2022-07-20 13:42:23 +0900

    Add a default UI
    * Added a button for checking class

--- SolidProject/ViewController.swift
+++ SolidProject/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private lazy var callButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("OCPButton", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+
+        button.backgroundColor = .secondarySystemBackground
+
+        button.addTarget(
+            self,
+            action: #selector(callButtonAction),
+            for: .touchUpInside
+        )
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setupViews()
     }
-
-
 }
 
+private extension ViewController {
+    func setupViews() {
+        [
+            callButton
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+
+        callButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        callButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+    @objc func callButtonAction() {
+        let dog = Dog()
+        let cat = Cat()
+        dog.makeSound()
+        cat.makeSound()
+    }
+}
