eea2abc58ac9928add947119bc79424e2df20790
Yoonjiwon <k112751@koreainvestment.com>
2022-07-21 13:12:18 +0900

    Add a menuCollectionView
    * Added a collectionView
    * Set collectionView datasource, delegate

--- SolidProject/Scene/PageViewController.swift
+++ SolidProject/Scene/PageViewController.swift
@@ -9,8 +9,75 @@ import Foundation
 import UIKit
 
 final class PageViewController: UIViewController {
+    private lazy var menuCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 5
+        layout.itemSize = CGSize(
+            width: UIScreen.main.bounds.width / 2 - 5,
+            height: 50.0
+        )
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: layout
+        )
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(
+            UICollectionViewCell.self,
+            forCellWithReuseIdentifier: "cell"
+        )
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupViews()
+    }
+}
+
+extension PageViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 2
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+extension PageViewController: UICollectionViewDelegateFlowLayout {
+
+}
+
+
+private extension PageViewController {
+    func setupViews() {
+        [
+            menuCollectionView
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+
+        menuCollectionView.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 0.0
+        ).isActive = true
+        menuCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        menuCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        menuCollectionView.heightAnchor.constraint(equalToConstant: 60.0).isActive = true
     }
 }
